40b6a5c21d1446fea9a87b720f9e61c669888846
Josue Igiraneza <igiranezaj28@gmail.com>
2025-02-16 14:02:31 +0200

    refactor: improve login page UI and input styling

--- app/login.tsx
+++ app/login.tsx
@@ -3,16 +3,20 @@ import { Text, TextInput, View } from 'react-native';
 
 const Login = () => {
   return (
-    <View className="bg-primaryDark flex-1">
+    <View className="flex-1 bg-primaryDark">
       <Text className="text-white">Login</Text>
       <View className="absolute bottom-0 h-1/2 w-full rounded-t-3xl bg-white p-5">
-        <Text className="text-primaryDark font-geistSemiBold text-3xl">Login</Text>
+        <Text className="font-geistSemiBold text-3xl text-primaryDark">Login</Text>
         <Text className="text-geistRegular my-2 text-base text-gray-500">
           Enter your email and password below to login to your account.
         </Text>
-        <View>
-          <Text className="font-geistRegular font-semibold">E-mail</Text>
-          <TextInput className="w-full border-2 bg-green-200 p-4" placeholder="m@example.com" />
+        <View className="my-4">
+          <Text className="my-2 font-geistRegular font-semibold">E-mail</Text>
+          <TextInput className="w-full rounded-lg border border-gray-300 bg-green-200/10 p-4" />
+        </View>
+        <View className="my-4">
+          <Text className="my-2 font-geistRegular font-semibold">Password</Text>
+          <TextInput className="w-full rounded-lg border border-gray-300 bg-green-200/10 p-4" />
         </View>
       </View>
     </View>
