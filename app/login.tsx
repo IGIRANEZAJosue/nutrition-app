06ce0d9e5d58786ac205eed35272b745aa2cb5c3
Josue Igiraneza <igiranezaj28@gmail.com>
2025-02-16 14:03:54 +0200

    refactor: adjust login page layout and spacing

--- app/login.tsx
+++ app/login.tsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <View className="flex-1 bg-primaryDark">
       <Text className="text-white">Login</Text>
-      <View className="absolute bottom-0 h-1/2 w-full rounded-t-3xl bg-white p-5">
+      <View className="min-h-1/2 absolute bottom-0 w-full rounded-t-3xl bg-white p-5">
         <Text className="font-geistSemiBold text-3xl text-primaryDark">Login</Text>
         <Text className="text-geistRegular my-2 text-base text-gray-500">
           Enter your email and password below to login to your account.
@@ -14,7 +14,7 @@ const Login = () => {
           <Text className="my-2 font-geistRegular font-semibold">E-mail</Text>
           <TextInput className="w-full rounded-lg border border-gray-300 bg-green-200/10 p-4" />
         </View>
-        <View className="my-4">
+        <View className="mb-4">
           <Text className="my-2 font-geistRegular font-semibold">Password</Text>
           <TextInput className="w-full rounded-lg border border-gray-300 bg-green-200/10 p-4" />
         </View>
