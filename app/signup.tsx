b077f6cd99be64201d02dd85b5bdc5fccf54ff31
Josue Igiraneza <igiranezaj28@gmail.com>
2025-02-19 20:02:49 +0200

    refactor: increase logo height on login and signup screens

--- app/signup.tsx
+++ app/signup.tsx
@@ -5,7 +5,7 @@ const signup = () => {
   return (
     <View className="flex-1 items-center bg-primaryDark">
       <Text className="text-center font-geistBold text-4xl text-white">Signup</Text>
-      <Image className="my-16 h-24" resizeMode="contain" source={require('../assets/logo.png')} />
+      <Image className="my-16 h-28" resizeMode="contain" source={require('../assets/logo.png')} />
     </View>
   );
 };
