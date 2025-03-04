322512806bdd03135c4fa28a29be101300882b93
Josue Igiraneza <igiranezaj28@gmail.com>
2025-03-04 19:51:01 +0200

    refactor: use tailwind instead of stylesheet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,13 @@
-import { StyleSheet, Text } from 'react-native';
+import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Header from '~/components/Header';
 
 export default function Home() {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView className="flex-1 p-5">
       <Header />
       <Text className="text-center font-geistBold text-3xl text-primaryDark">Home</Text>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
