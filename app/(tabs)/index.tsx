5426b89a1bd3e0b7dfd4ea0780d3a61b3c00e2a7
Josue Igiraneza <igiranezaj28@gmail.com>
2025-03-06 12:08:05 +0200

    feat: add sleep tracking display card to home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,6 +10,17 @@ export default function Home() {
       <View className="my-4 gap-1">
         <Text className="font-geistMedium text-2xl text-gray-600">Hello,</Text>
         <Text className="font-geistSemiBold text-3xl">Josue Igiraneza</Text>
+
+        <View className="my-4 w-full flex-row items-center justify-between rounded-2xl border border-gray-300 p-4">
+          <View className="gap-4">
+            <Text className="font-geistMedium text-xl">Sleep</Text>
+            <Text className="font-geistBold text-2xl">6.5 hrs</Text>
+            <Text className="font-geistRegular text-xl text-gray-400">/8hrs</Text>
+          </View>
+          <View className="size-28 items-center justify-center rounded-full border-8 border-primary">
+            <Text>80%</Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
