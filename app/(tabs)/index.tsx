64d05803fbc31637680182b0d769fa2c6ada79b4
Josue Igiraneza <igiranezaj28@gmail.com>
2025-03-10 11:47:00 +0200

    feat: add icon in activity card and enhance home page layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,10 +8,11 @@ export default function Home() {
   return (
     <SafeAreaView className="flex-1 bg-gray-100 p-5">
       <Header />
-      <View className="my-4 gap-1">
-        <Text className="font-geistMedium text-2xl text-gray-600">Hello,</Text>
-        <Text className="font-geistSemiBold text-3xl">Josue Igiraneza</Text>
-
+      <View className="my-4 gap-4">
+        <View>
+          <Text className="font-geistMedium text-2xl text-gray-600">Hello,</Text>
+          <Text className="font-geistSemiBold text-3xl">Josue Igiraneza</Text>
+        </View>
         <ActivityCard title="sleep" value={6.5} target={8} percentage={80} />
         <ActivityCard title="steps" value={5000} target={7500} percentage={80} />
       </View>
--- components/ActivityCard.tsx
+++ components/ActivityCard.tsx
@@ -1,3 +1,4 @@
+import { Footprints, FootprintsIcon } from 'lucide-react-native';
 import React from 'react';
 import { Text, View } from 'react-native';
 
@@ -12,7 +13,12 @@ const ActivityCard = ({ title, value, target, percentage }: ActivityCardProps) =
   return (
     <View className="w-full flex-row items-center justify-between rounded-2xl border border-gray-300 bg-white p-4">
       <View className="gap-4">
-        <Text className="font-geistMedium text-xl capitalize">{title}</Text>
+        <View className="flex-row items-center gap-3">
+          <View className="size-12 items-center justify-center rounded-full bg-primaryLight">
+            <Footprints size={20} />
+          </View>
+          <Text className="font-geistMedium text-xl capitalize">{title}</Text>
+        </View>
         <Text className="font-geistBold text-3xl">{value}</Text>
         <Text className="font-geistRegular text-xl text-gray-400">/{target}</Text>
       </View>
