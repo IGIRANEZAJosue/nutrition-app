661863a0319431eed2d4392ca02d75233051b898
Josue Igiraneza <igiranezaj28@gmail.com>
2025-05-03 18:29:32 +0200

    feat: display user's name in the home screen if available

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,14 +3,17 @@ import { Text, View } from 'react-native';
 import ActivityCard from '~/components/ActivityCard';
 import CircularProgressBar from '~/components/CircularProgressBar';
 import { Container } from '~/components/Container';
+import { useAuth } from '~/context/AuthContext';
 
 export default function Home() {
+  const { user } = useAuth();
+
   return (
     <Container>
       <View className="my-4 gap-4">
         <View>
           <Text className="font-geistMedium text-2xl text-gray-600">Hello,</Text>
-          <Text className="font-geistSemiBold text-3xl">Josue Igiraneza</Text>
+          <Text className="font-geistSemiBold text-3xl">{user ? user.name : 'Friend'}</Text>
         </View>
         <ActivityCard title="sleep" value={6.5} target={8} percentage={80} />
         <ActivityCard title="steps" value={5000} target={7500} percentage={50} />
