6cad47686d45272b05bd29d6d45ff5157fb944e3
Josue Igiraneza <igiranezaj28@gmail.com>
2025-05-18 21:46:13 +0200

    refactor: rename AnalyticsCard into MetricCard for better understanding

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { Text, View } from 'react-native';
 
 import CircularProgressBar from '~/components/CircularProgressBar';
 import { Container } from '~/components/Container';
-import ActivityCard from '~/components/cards/ActivityCard';
+import MetricCard from '~/components/cards/MetricCard';
 import { useAuth } from '~/context/AuthContext';
 
 export default function Home() {
@@ -15,8 +16,8 @@ export default function Home() {
           <Text className="font-geistMedium text-2xl text-gray-600">Hello,</Text>
           <Text className="font-geistSemiBold text-3xl">{user ? user.name : 'Friend'}</Text>
         </View>
-        <ActivityCard title="sleep" value={6.5} target={8} percentage={80} />
-        <ActivityCard title="steps" value={5000} target={7500} percentage={50} />
+        <MetricCard title="sleep" value={6.5} target={8} percentage={80} />
+        <MetricCard title="steps" value={5000} target={7500} percentage={50} />
         <CircularProgressBar />
       </View>
     </Container>
