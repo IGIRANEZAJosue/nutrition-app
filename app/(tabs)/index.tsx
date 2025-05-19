25976d5db6d6e3a052a4edf81de3ae18029fe252
Josue Igiraneza <igiranezaj28@gmail.com>
2025-05-19 20:31:18 +0200

    feat: move health stats cards to their own components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { Text, View } from 'react-native';
 import CircularProgressBar from '~/components/CircularProgressBar';
 import { Container } from '~/components/Container';
 import MetricCard from '~/components/cards/MetricCard';
+import HealthStatsCards from '~/components/sections/HealthStatsCards';
 import RingProgress from '~/components/ui/RingProgress';
 import { useAuth } from '~/context/AuthContext';
 
@@ -25,39 +26,7 @@ export default function Home() {
         </View>
 
         {/* Health Stats Cards  */}
-        <View className="mb-6 flex-row flex-wrap justify-between">
-          <View className="mb-4 w-[48%] rounded-xl border border-gray-200 bg-[#F8F9FA] p-4">
-            <View className="mb-2 flex-row items-center">
-              <Text className="mr-2 text-red-500">❤️</Text>
-              <Text className="font-geistRegular text-sm text-gray-600">Heart Rate</Text>
-            </View>
-            <Text className="font-geistSemiBold text-xl">72 BPM</Text>
-          </View>
-
-          <View className="mb-4 w-[48%] rounded-xl border border-gray-200 bg-[#F8F9FA] p-4">
-            <View className="mb-2 flex-row items-center">
-              <Text className="mr-2">🛏️</Text>
-              <Text className="font-geistRegular text-sm text-gray-600">Sleep</Text>
-            </View>
-            <Text className="font-geistSemiBold text-xl">7h 23m</Text>
-          </View>
-
-          <View className="mb-4 w-[48%] rounded-xl border border-gray-200 bg-[#F8F9FA] p-4">
-            <View className="mb-2 flex-row items-center">
-              <Text className="mr-2">🔥</Text>
-              <Text className="font-geistRegular text-sm text-gray-600">Calories</Text>
-            </View>
-            <Text className="font-geistSemiBold text-xl">847 kcal</Text>
-          </View>
-
-          <View className="mb-4 w-[48%] rounded-xl border border-gray-200 bg-[#F8F9FA] p-4">
-            <View className="mb-2 flex-row items-center">
-              <Text className="mr-2">🏃</Text>
-              <Text className="font-geistRegular text-sm text-gray-600">Activity</Text>
-            </View>
-            <Text className="font-geistSemiBold text-xl">42 min</Text>
-          </View>
-        </View>
+        <HealthStatsCards />
 
         <MetricCard title="sleep" value={6.5} target={8} percentage={80} />
         <MetricCard title="steps" value={5000} target={7500} percentage={50} />
